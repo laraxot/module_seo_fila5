aba19804247a5867f456bc846718751aee6967e2
marco76tv <marco.sottana@gmail.com>
2026-04-21 08:03:53 +0200

    .

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,10 +2,4 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 uses()->group('seo');
-=======
-/** @phpstan-ignore-next-line */
-uses(Modules\Seo\Tests\TestCase::class)->in(__DIR__);
-
->>>>>>> b33919b (.)
