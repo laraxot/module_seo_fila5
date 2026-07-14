e80580640ee54f7022242ee44437651942821b80
marco76tv <marco.sottana@gmail.com>
2026-07-14 20:23:06 +0200

    .

--- tests/Unit/Datas/SocialShareDataTest.php
+++ tests/Unit/Datas/SocialShareDataTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Modules\Seo\Tests\Unit\Data;
+namespace Modules\Seo\Tests\Unit\Datas;
 
-use Modules\Seo\Data\SocialShareData;
+use Modules\Seo\Datas\SocialShareData;
 use PHPUnit\Framework\Assert;
 
 uses(\Modules\Seo\Tests\TestCase::class);
